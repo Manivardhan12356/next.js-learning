318249f9627650febc4e79615b6ed8038a749e8c
MANI VARDHAN <91270428+Manivardhan12356@users.noreply.github.com>
2025-07-07 21:56:29 +0530

    array string for catch all

--- src/app/routes/catchAll/[[...slug]]/page.tsx
+++ src/app/routes/catchAll/[[...slug]]/page.tsx
@@ -1,17 +1,30 @@
-interface Props {
-  params: {
-    slug: string;
-  };
-}
+// app/blog/[...slug]/page.tsx
 
-export default function BlogPostPage({ params }: Props) {
+import React from 'react';
+
+type Props = {
+  params: {
+    slug?: string[]; // slug can be undefined or an array of strings
+  };
+};
+
+const CatchAllSegments = ({ params }: Props) => {
   const { slug } = params;
 
-  return (
-    <main>
-      <h1>Blog Post: {slug}</h1>
-      {/* You can fetch post data here using the slug */}
-    </main>
-  );
-}
+  if (!slug) {
+    return <div>No slug provided</div>;
+  }
+
+  if (slug.length === 1) {
+    return <h1>{slug[0]}</h1>;
+  }
+
+  if (slug.length === 2) {
+    return <h1>{`${slug[0]} and ${slug[1]}`}</h1>;
+  }
+
+  return <div>CatchAllSegments: {slug.join(' / ')}</div>;
+};
+
+export default CatchAllSegments;
 
